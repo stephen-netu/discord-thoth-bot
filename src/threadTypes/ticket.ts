e462be7e987290e32627c99b5da167115fc41df8
Stephen Wooten II <stephen.netu@gmail.com>
2022-01-01 15:56:58 -0500

    ticket.ts
    Changing to more appropriate titling

--- src/threadTypes/ticket.ts
+++ src/threadTypes/ticket.ts
@@ -1,23 +1,23 @@
 import { Client, Message, MessageEmbed, TextChannel } from "discord.js";
 
-export async function createTicket(client: Client, message: Message, to: string) {
+export async function createFeatureRequest(client: Client, message: Message, to: string) {
+    const title = message.content.toString().slice(0, 28) + "...";
     const content = message.content;
     const authorText = `${message.author.username}#${message.author.discriminator}`;
-    const threadName = `${authorText}`;
     const author = `<@!${message.author.id}>`;
     await message.delete();
 
     const channel = client.channels.cache.get(to) as TextChannel;
     const embed = new MessageEmbed({
-        title: `New Idea - ${authorText}`,
+        title: `New Idea by ${authorText}`,
         description: `${content}`,
-        color: "AQUA",
+        color: "LUMINOUS_VIVID_PINK",
         footer: {
-            text: `Posted by ${message.author.tag}.`,
+            text: `Created by ${message.author.tag}. Please provide any additional details in the new thread.`,
             iconURL: message.author.avatarURL(),
         },
     });
 
     const newMessage = await channel.send({ embeds: [embed], content: `${author}` });
-    newMessage.startThread({ name: threadName, rateLimitPerUser: 50, reason: `New Idea` });
+    newMessage.startThread({ name: title, rateLimitPerUser: 50, reason: `New Idea Submission` });
 }
