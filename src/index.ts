4741614082827eec4fb5c2f0989d0de0ed6fc212
Stephen Wooten II <stephen.netu@gmail.com>
2022-01-01 13:23:03 -0500

    v1
    NbdyAsst changes for new server

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,21 @@ import { Client, Intents } from "discord.js";
 import { ActivityTypes } from "discord.js/typings/enums";
 import { getClientToken } from "./system/config";
 import { Threader } from "./system/threader";
-import { createBugReport } from "./threadTypes/bug";
-import { createFeatureRequest } from "./threadTypes/feature";
 import { createTicket } from "./threadTypes/ticket";
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const supportTicketChannels = {
-    from: "926540793701888081",
-    to: "926541432892821514",
-};
-
-const bugTicketChannels = {
-    from: "918918419607330816",
-    to: "914695154751905852",
-};
-
-const featureTicketChannels = {
-    from: "926544271539077120",
-    to: "918166438617960518",
+    from: "860655061460582460",
+    to: "826265807210283037",
 };
 
 client.on("ready", () => {
-    client.user.setActivity("Support Requests", { type: ActivityTypes.LISTENING });
+    client.user.setActivity("Ideas Submissions", { type: ActivityTypes.LISTENING });
     client.on("messageCreate", Threader.handle);
     Threader.init(client);
     Threader.bind(supportTicketChannels.from, supportTicketChannels.to, createTicket);
-    Threader.bind(bugTicketChannels.from, bugTicketChannels.to, createBugReport);
-    Threader.bind(featureTicketChannels.from, featureTicketChannels.to, createFeatureRequest);
-    console.log(`[${Date.now()}] Started Thoth Successfully. Listening for Messages.`);
+    console.log(`[${Date.now()}] Started NbdyAsst Successfully. Listening for Messages.`);
 });
 
 client.login(getClientToken());
