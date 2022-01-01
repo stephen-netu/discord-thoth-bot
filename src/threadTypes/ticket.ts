41f48a347ebb8ac82bacc0c4a3191d37e41af072
Stephen Wooten II <stephen.netu@gmail.com>
2022-01-01 13:12:29 -0500

    Update ticket.ts
    Changing "tickets" to "ideas"

--- src/threadTypes/ticket.ts
+++ src/threadTypes/ticket.ts
@@ -3,21 +3,21 @@ import { Client, Message, MessageEmbed, TextChannel } from "discord.js";
 export async function createTicket(client: Client, message: Message, to: string) {
     const content = message.content;
     const authorText = `${message.author.username}#${message.author.discriminator}`;
-    const threadName = `${authorText} Ticket`;
+    const threadName = `${authorText}`;
     const author = `<@!${message.author.id}>`;
     await message.delete();
 
     const channel = client.channels.cache.get(to) as TextChannel;
     const embed = new MessageEmbed({
-        title: `Support Ticket for ${authorText}`,
+        title: `New Idea - ${authorText}`,
         description: `${content}`,
         color: "AQUA",
         footer: {
-            text: `Requested by ${message.author.tag}. If someone is able to answer your question they will write in this thread. Please be patient.`,
+            text: `Posted by ${message.author.tag}.`,
             iconURL: message.author.avatarURL(),
         },
     });
 
     const newMessage = await channel.send({ embeds: [embed], content: `${author}` });
-    newMessage.startThread({ name: threadName, rateLimitPerUser: 50, reason: `Support Ticket Request` });
+    newMessage.startThread({ name: threadName, rateLimitPerUser: 50, reason: `New Idea` });
 }
